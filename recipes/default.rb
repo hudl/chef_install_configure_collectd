77e6000f39d3e155e33914c6a9c88f036c60e9b0
Luke Eisenbraun <luke-eisenbraun@users.noreply.github.com>
2021-04-08 15:41:33 -0500

    Sync github with chef server

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,7 +26,35 @@ node.default['collectd_conf_folder'] = get_collectd_conf_folder
 node.default['collectd_managed_conf_folder'] = "#{node['collectd_conf_folder']}/managed_config"
 node.default['collectd_filtering_conf_folder'] = "#{node['collectd_conf_folder']}/filtering_config"
 
-include_recipe 'chef_install_configure_collectd::install-collectd'
+if node[:hudl].fetch(:patch_signalfx_installation, false)
+  remote_file '/tmp/SignalFx-collectd-RPMs-AWS_EC2_Linux-release-latest.noarch.rpm' do
+    source 'https://dl.signalfx.com/rpms/SignalFx-rpms/release/SignalFx-collectd-RPMs-AWS_EC2_Linux-release-latest.noarch.rpm'
+    action :create
+  end
+
+  remote_file '/tmp/SignalFx-collectd_plugin-RPMs-AWS_EC2_Linux-release-latest.noarch.rpm' do
+    source 'https://dl.signalfx.com/rpms/SignalFx-rpms/release/SignalFx-collectd_plugin-RPMs-AWS_EC2_Linux-release-latest.noarch.rpm'
+    action :create
+  end
+
+  yum_package 'signalfx-collectd-repo' do
+    source '/tmp/SignalFx-collectd-RPMs-AWS_EC2_Linux-release-latest.noarch.rpm'
+    action :install
+  end
+
+  yum_package 'signalfx-collectd-plugins-repo' do
+    source '/tmp/SignalFx-collectd_plugin-RPMs-AWS_EC2_Linux-release-latest.noarch.rpm'
+    action :install
+  end
+
+  ['collectd', 'collectd-disk'].each do |package|
+    yum_package package do
+      action :install
+    end
+  end
+else
+  include_recipe 'chef_install_configure_collectd::install-collectd'
+end
 include_recipe 'chef_install_configure_collectd::config-collectd'
 include_recipe 'chef_install_configure_collectd::config-signalfx'
 
